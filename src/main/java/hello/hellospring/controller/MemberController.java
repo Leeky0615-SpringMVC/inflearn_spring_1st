3a21ffced691d1cad989888939cb366a44963dc3
Leeky <rbdus7174@naver.com>
2021-03-19 17:40:03 +0900

    0319

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,47 @@
+package hello.hellospring.controller;
+
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+// 스프링 컨테이너가 뜰 떄, 이 어노테이션이 있으면 멤버컨르롤러 객체를 생성해서 넣어둔다.
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    // 스프링 컨테이너에 있는 멤버서비스를 가져다가 연결을 시켜준다.
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
